e3e0c902649c2af68f89500ace0303022abcdb3f
catalamarti <marticatalasabate@gmail.com>
2025-03-19 08:22:49 +0000

    Update CodeToRun.R

--- Study/CodeToRun.R
+++ Study/CodeToRun.R
@@ -4,31 +4,21 @@ renv::activate()
 renv::restore()
 
 # create connection
-server_dbi <- "cdm_gold_202407"
-port<-Sys.getenv("DB_PORT")
-host<-Sys.getenv("DB_HOST")
-user<-Sys.getenv("DB_USER")
-password<-Sys.getenv("DB_PASSWORD")
-con <- DBI::dbConnect(RPostgres::Postgres(),
-                       dbname = server_dbi,
-                       port = port,
-                       host = host,
-                       user = user,
-                       password = password)
+con <- DBI::dbConnect("...")
 
 # database acronym (e.g. "CPRD AURUM")
-dbName <- "CPRD GOLD 100k"
+dbName <- "..."
 
 # schemas and prefix
-cdmSchema <- "public_100k"
-writeSchema <- "results"
-writePrefix <- "mcs_"
+cdmSchema <- "..."
+writeSchema <- "..."
+writePrefix <- "..."
 
 # suppression
 minCellCount <- 5L
 
 # data cut
-dataEndDate <- "2024-07-01"
+dataEndDate <- "..."
 censorAge <- 150L
 
 source(here::here("RunAnalysis.R"))
