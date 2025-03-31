6c5640932bb2d6aa02dbf67d537f80df6c45d0c4
catalamarti <marticatalasabate@gmail.com>
2025-03-31 16:37:23 +0100

    abstract

--- Report/data/preprocess.R
+++ Report/data/preprocess.R
@@ -1,6 +1,43 @@
 # shiny is prepared to work with this resultList, please do not change them
 result <- omopgenerics::importSummarisedResult(file.path(getwd(), "data"))
 
+mode <- dplyr::tribble(
+  ~mode, ~new_mode,
+  "original_data", "Original",
+  "min_extract", "First record to extract",
+  "min_max", "First to last record",
+  "impatient", "Inpatient hospitalisation",
+  "persistence_0_surveillanceFALSE", "Persistence 0",
+  "persistence_180_surveillanceFALSE", "Persistence 180",
+  "persistence_180_surveillanceTRUE", "Persistence 180 + surveillance",
+  "persistence_365_surveillanceFALSE", "Persistence 365",
+  "persistence_365_surveillanceTRUE", "Persistence 365 + surveillance",
+  "persistence_545_surveillanceFALSE", "Persistence 545",
+  "persistence_545_surveillanceTRUE", "Persistence 545 + surveillance",
+  "persistence_730_surveillanceFALSE", "Persistence 730",
+  "persistence_730_surveillanceTRUE", "Persistence 730 + surveillance"
+)
+
+cdmNames <- dplyr::tribble(
+  ~cdm_name, ~new_cdm_name,
+  "Barts Health", "Barts Health",
+  "CPRD AURUM 202409 - 50 prac", "CPRD AURUM 50",
+  "GOSH_OMOP", "GOSH",
+  "IDRIL_1", "IDRIL",
+  "UCLH-from-2019", "UCLH"
+)
+
+result <- result |>
+  dplyr::inner_join(cdmNames, by = "cdm_name") |>
+  dplyr::select(!"cdm_name") |>
+  dplyr::rename("cdm_name" = "new_cdm_name") |>
+  omopgenerics::newSummarisedResult(
+    settings = omopgenerics::settings(result) |>
+      dplyr::inner_join(mode, by = "mode") |>
+      dplyr::select(!"mode") |>
+      dplyr::rename("mode" = "new_mode")
+  )
+
 data <- c(
   "snapshot" = "summarise_omop_snapshot",
   "in_obs" = "summarise_in_observation",
--- Report/plotsAbstract.R
+++ Report/plotsAbstract.R
@@ -31,29 +31,23 @@ characteristics <- data$obs_period |>
   ) |>
   dplyr::filter(
     !.data$estimate_name %in% c("mean", "sd", "density_x", "density_y"),
-    !.data$variable_name %in% c(
-      "sex", "age_group_start", "age_group_end", "number_drugs",
-      "number_conditions", "number_visits"
-    ),
-    .data$estimate_name != "Range" | .data$variable_name %in% c(
-      "duration", "time_to_next_observation", "op_per_person"
-    )
+    .data$estimate_name != "Range"
   ) |>
   dplyr::select(!c("result_id", "variable_level")) |>
-  dplyr::left_join(
+  dplyr::inner_join(
     dplyr::tribble(
       ~variable_name, ~new_variable_name,
       "number records", "Number records",
       "number subjects", "Number subjects",
       "duration", "OP length",
-      "time_to_next_observation", "Time to next OP",
+      "time_to_next_observation", "Time between OP",
       "op_per_person", "OP per person",
-      "age_start", "Age (start)",
-      "age_end", "Age (end)",
-      "Female", "Female",
-      "drugs_per_day", "Drugs per 1,000 days",
-      "conditions_per_day", "Conditions per 1,000 days",
-      "visits_per_day", "Visits per 1,000 days"
+      #"age_start", "Age (start)",
+      #"age_end", "Age (end)",
+      #"Female", "Female",
+      #"drugs_per_day", "Drugs per 1,000 days",
+      #"conditions_per_day", "Conditions per 1,000 days",
+      #"visits_per_day", "Visits per 1,000 days"
     ) |>
       dplyr::mutate(order_id = dplyr::row_number()),
     by = "variable_name"
@@ -62,29 +56,115 @@ characteristics <- data$obs_period |>
   dplyr::arrange(.data$order_id) |>
   dplyr::select(!c("variable_name", "order_id")) |>
   dplyr::rename(variable_name = "new_variable_name") |>
-  dplyr::relocate("variable_name")
+  dplyr::relocate("variable_name") |>
+  dplyr::filter(!grepl("180|545", .data$mode))
 
-visOmopResults::visTable(
+gt <- visOmopResults::visTable(
   result = characteristics,
-  header = "mode",
-  groupColumn = "cdm_name",
-  hide = "estimate_type"
+  header = "cdm_name",
+  groupColumn = "mode",
+  hide = c("estimate_name", "estimate_type")
 )
+
+gt::gtsave(gt, here::here("figures", "obs.docx"))
 
 x <- data$in_obs |>
   dplyr::filter(
     .data$variable_name == "Number person-days",
-    .data$age_group %in% c("overall", "0 to 19", "80 or above"),
+    .data$age_group == "overall",
     .data$sex == "overall",
-    .data$year >= 2012L
+    .data$year >= 2012L,
+    !grepl("180|545", .data$mode)
+  )
+x <- x |>
+  dplyr::union_all(
+    x |>
+      dplyr::filter(cdm_name == "CPRD AURUM 50", mode == "Original") |>
+      dplyr::mutate(count = 0L, mode = "Inpatient hospitalisation")
   ) |>
-  dplyr::group_by(.data$mode, .data$age_group, .data$cdm_name) |>
-  dplyr::summarise(total = sum(.data$count), .groups = "drop") |>
-  dplyr::group_by(.data$mode) |>
-  dplyr::mutate(id = dplyr::cur_group_id()) |>
-  dplyr::ungroup() |>
-  dplyr::filter(!is.na(.data$total))
+  dplyr::mutate(
+    `person-years` = .data$count / 365,
+    `OP definition` = .data$mode
+  ) |>
+  dplyr::arrange(`OP definition`)
 
-ggplot2::ggplot(data = x, mapping = ggplot2::aes(x = mode, y = total, colour = age_group)) +
+p0 <- ggplot2::ggplot(
+  data = x, mapping = ggplot2::aes(x = year, y = `person-years`, colour = `OP definition`)
+) +
+  ggplot2::geom_line() +
+  ggplot2::facet_wrap("cdm_name")
+
+plotx <- function(x) {
+  ggplot2::ggplot(
+    data = x, mapping = ggplot2::aes(x = year, y = `person-years`, colour = `OP definition`)
+  ) +
+    ggplot2::geom_line() +
+    ggplot2::facet_wrap("cdm_name") +
+    ggplot2::theme(legend.position = "top")
+}
+
+p1 <- x |>
+  dplyr::filter(cdm_name == "Barts Health") |>
+  plotx() +
+  ggplot2::xlim(c(2012, 2024)) +
+  ggplot2::ylim(c(0, 3e6))
+
+p2 <- x |>
+  dplyr::filter(cdm_name == "IDRIL") |>
+  plotx() +
+  ggplot2::xlim(c(2012, 2024)) +
+  ggplot2::ylim(c(0, 1.5e6))
+
+p3 <- x |>
+  dplyr::filter(cdm_name == "UCLH") |>
+  plotx() +
+  ggplot2::xlim(c(2019, 2024)) +
+  ggplot2::ylim(c(0, 1.2e6))
+
+p4 <- x |>
+  dplyr::filter(cdm_name == "GOSH") |>
+  plotx() +
+  ggplot2::xlim(c(2018, 2024)) +
+  ggplot2::ylim(c(0, 0.21e6))
+
+p5 <- x |>
+  dplyr::filter(cdm_name == "CPRD AURUM 50") |>
+  plotx() +
+  ggplot2::xlim(c(2012, 2024)) +
+  ggplot2::ylim(c(0, 1.75e6))
+
+
+library(patchwork)
+
+p <- p1 + p2 + p3 + p4 + p5 +
+  plot_layout(guides = "collect") &  # Collect legends
+  theme(legend.position = "top")
+
+x <- data$incidence |>
+  dplyr::filter(
+    .data$outcome == "ciprofloxacin",
+    cdm_name != "CPRD AURUM 50",
+    year == 2023, sex == "overall", age_group == "overall",
+    grepl("incidence", .data$estimate_name)
+  ) |>
+  omopgenerics::pivotEstimates() |>
+  dplyr::rename(`Observation period definition` = "mode", `Incidence per 100,000 py` = "incidence_100000_pys")
+
+ggplot2::ggplot(
+  data = x,
+  mapping = ggplot2::aes(
+    x = `Observation period definition`,
+    y = `Incidence per 100,000 py`,
+    ymax = incidence_100000_pys_95CI_upper,
+    ymin = incidence_100000_pys_95CI_lower,
+    colour = `Observation period definition`
+  )
+) +
   ggplot2::geom_point() +
-  ggplot2::facet_grid(cdm_name ~ ., scales = "free")
+  ggplot2::geom_errorbar() +
+  ggplot2::facet_wrap("cdm_name", scales = "free_y") +
+  ggplot2::scale_y_log10() +
+  ggplot2::theme(
+      axis.ticks.x = ggplot2::element_blank(),
+      axis.text.x = ggplot2::element_blank()
+  )
